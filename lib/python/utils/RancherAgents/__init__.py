9e33c7d802fa0acdb5bbb11607a3f607240fe44d
moelsayed <m.elsayed@gmail.com>
2017-08-16 01:01:54 +0200

    rename env variable

--- lib/python/utils/RancherAgents/__init__.py
+++ lib/python/utils/RancherAgents/__init__.py
@@ -256,7 +256,7 @@ class RancherAgents(object):
                 agent_count = int(str(os.environ['RANCHER_AGENTS_COUNT']).rstrip())
                 agent_prefix = self.__agent_name_prefix()
                 region = str(os.environ['AWS_DEFAULT_REGION']).rstrip()
-                reg_command = str(os.environ['REGISTRATION_COMMAND']).rstrip()
+                reg_command = str(os.environ['RANCHER_REGISTRATION_COMMAND']).rstrip()
                 agent_os = str(os.environ['RANCHER_AGENT_OPERATINGSYSTEM']).rstrip() 
                 os_settings = os_to_settings(agent_os)                               
                 ssh_user = os_settings['ssh_username']                               
@@ -264,10 +264,6 @@ class RancherAgents(object):
                 try:
                         self.__ensure_rancher_agents()
                         self.__ensure_agents_docker()
-                        if reg_command:
-                            agent_name = agent_prefix + str(agent)              
-                            addr = ec2_node_public_ip(agent_name, region=region)
-                            SSH(agent_name, addr, ssh_user, reg_command)        
 
                 except RancherAgentsError as e:
                         msg = "Failed while provisioning Rancher Agents!: {}".format(str(e))
@@ -278,6 +274,9 @@ class RancherAgents(object):
                     agent_name = agent_prefix + str(agent)
                     addr = ec2_node_public_ip(agent_name, region=region)
                     log_info("Standalone Agent {}: {}".format(agent_name, addr))
+                    if reg_command:
+                        SSH(agent_name, addr, ssh_user, reg_command)        
+
                 return True
 
         #
